84e9d3939b73beffae72a9902c449ca682fee2b7
Vladimir <w112vovan@mail.ru>
2023-11-29 15:48:42 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L24_subarrayOfRepeatedNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mainArray = { 1, 1, 2, 2, 3, 3, 3, 2, 2, 7, 7, 7, 7, 7, 7, 5, 5, 5, 9, 9, 9, 9, 4, 4, 4, 7, 7, 6, 6, 6 };
+            int foundNumber = int.MinValue;
+            int maxCounter = 0;
+            int currentCounter = 1;
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < mainArray.Length - 1; i++)
+            {
+                if (mainArray[i] == mainArray[i + 1])
+                {
+                    currentCounter++;
+                }
+                else
+                {
+                    if (maxCounter < currentCounter)
+                    {
+                        maxCounter = currentCounter;
+                        foundNumber = mainArray[i];
+
+                    }
+                    currentCounter = 1;
+                }
+                Console.Write(mainArray[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Число {foundNumber} повторяется {maxCounter} раз подряд.");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("L24_subarrayOfRepeatedNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L24_subarrayOfRepeatedNumbers")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("aba41544-1596-423c-8a66-98abf162254a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
